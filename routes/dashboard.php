0452c265b671afdfc0bedbc41849064d57d1a49f
bunyamin88 <eskrimadu@gmail.com>
2025-12-09 20:15:45 +0700

    redirect route to controller

--- routes/dashboard.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
-
-Route::get('/', [Dashboard::class, 'render'])->name('dashboard');
--- routes/web.php
+++ routes/web.php
@@ -2,32 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Dashboard\DistributionController;
+use App\Http\Controllers\Dashboard\DetailController;
+use App\Http\Controllers\Dashboard\FilterController;
+use App\Http\Controllers\Dashboard\BusinessController;
+use App\Http\Controllers\Dashboard\OrganizationController;
 use Laravel\Fortify\Features;
 
-    Route::get('/', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::get('/cooperatives', function () {
-        return Inertia::render('cooperatives/index');
-    })->name('cooperatives.index');
+    Route::get('/cooperatives', [FilterController::class, 'index'])->name('cooperatives.index');
+    
 
-    Route::get('/cooperatives/{id}', function ($id) {
-        return Inertia::render('cooperatives/show', [
-            'id' => $id
-        ]);
-    })->name('cooperatives.show');
+    Route::get('/cooperatives/{id}', [DetailController::class,'index'])->name('cooperatives.show');
 
-    Route::get('/business', function () {
-        return Inertia::render('business/index');
-    })->name('business.index');
+    Route::get('/distribution', [DistributionController::class, 'index'])->name('distribution.index');
 
-    Route::get('/organization', function () {
-        return Inertia::render('organization/index');
-    })->name('organization.index');
+    Route::get('/business', [BusinessController::class, 'index'])->name('business.index');
 
-    Route::get('/distribution', function () {
-        return Inertia::render('distribution/index');
-    })->name('distribution.index');
+    Route::get('/organization', [OrganizationController::class, 'index'])->name('organization.index');
 
 // require __DIR__.'/settings.php';
+
+//Route::get('/', [DashboardController::class, 'render'])->name('dashboard');
+//Route::get('/cooperative/{id}', [DetailController::class, 'render'])->name('dashboard.detail');
+//Route::get('/search', [SearchController::class, 'render'])->name('dashboard.search');
+// Route::get('/business', [BusinessController::class, 'render'])->name('dashboard.business');
+// Route::get('/organization', [OrganizationController::class, 'render'])->name('dashboard.organization');
