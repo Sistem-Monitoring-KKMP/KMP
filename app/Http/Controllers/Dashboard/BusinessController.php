72d237df283eb24f1ea4ced7c74e570d86e687a3
bunyamin88 <eskrimadu@gmail.com>
2025-12-09 20:14:45 +0700

    create controller

--- app/Http/Controllers/Dashboard/BusinessController.php
+++ app/Http/Controllers/Dashboard/BusinessController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Services\HistoryPerformService;
+use App\Services\KeuanganService;
+use App\Services\NeracaService;
+use Inertia\Inertia;
 
 class BusinessController extends Controller
 {
-  public function index()
+  public function index(KeuanganService $financial, NeracaService $neraca, HistoryPerformService $Bdi)
   {
-    // Logic for retrieving business data
+    $historyData = $financial->getAllKeuangan();
+    $neraca = $neraca->getAllNeraca();
+    $bdi = $Bdi->getAllBdi();
+    $latest = !empty($historyData) ? end($historyData) : null;
+
+    dd($latest);
+    
+    return Inertia::render('business/index', [
+      'ringkasan_finansial' => $latest,
+      'neraca' => $neraca,
+      'bdi_trend' => $bdi,
+      'pertumbuhan_financial' => $historyData,
+    ]);
   }
 }
--- app/Http/Controllers/Dashboard/DetailController.php
+++ app/Http/Controllers/Dashboard/DetailController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Services\DetailService;
+use Inertia\Inertia;
 
 class DetailController extends Controller
 {
-  public function index()
+  public function index(int $koperasiId, DetailService $detailService)
   {
-    // Logic for retrieving detail data
+    $detail = $detailService->getDetail($koperasiId);
+    dd($detail);
+
+    return Inertia::render('cooperatives/show', [
+    'data' => [$detail]
+    ]);
+
+    
+    
   }
 }
